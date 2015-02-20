aa2239b68398251eb89c8809748736e7b4ef7359
Wade Sellers <Waderoni@gmail.com>
2015-02-19 18:25:23 -0700

    Initial Commit

---
+++ CaptainsLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaptainsLog
+//
+//  Created by Wade Sellers on 2/19/15.
+//  Copyright (c) 2015 WadeSellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
