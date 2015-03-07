fec991b525b7b420f8354ce9538bcaa0ff933b73
Wade Sellers <Waderoni@gmail.com>
2015-03-07 16:24:30 -0700

    major overhaul

--- CaptainsLog/ViewController.m
+++ CaptainsLog/ViewController.m
@@ -10,15 +10,25 @@
 
 @interface ViewController () <UITextViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UIImageView *woodTableImage;
-@property (weak, nonatomic) IBOutlet UIImageView *logCoverImage;
+@property (weak, nonatomic) IBOutlet UIImageView *woodTable;
+@property (weak, nonatomic) IBOutlet UIImageView *logCover;
 @property (weak, nonatomic) IBOutlet UIImageView *quill;
 @property (weak, nonatomic) IBOutlet UIImageView *paper;
 @property (weak, nonatomic) IBOutlet UITextView *paperTextView;
 
 
-@property CGRect bookStartPosition;
-@property CGRect quillStartPosition;
+@property CGRect bookRestPosition;
+@property CGRect bookActivePosition;
+@property CGRect bookOpenPosition;
+@property CGRect quillRestPosition;
+@property CGRect quillActivePosition;
+@property CGRect woodTableRestPosition;
+@property CGRect woodTableActivePosition;
+@property CGRect paperRestPosition;
+@property CGRect paperActivePosition;
+@property CGRect paperTextViewRestPosition;
+@property CGRect paperTextViewActivePosition;
+
 @end
 
 @implementation ViewController
@@ -28,6 +38,7 @@
 
 //    NSUserDefaults *log = [NSUserDefaults standardUserDefaults];
 //    self.paperTextView.text = [log stringForKey:@"textLog"];
+    
 
     
 
@@ -35,10 +46,36 @@
 
 - (void)viewDidAppear:(BOOL)animated
 {
+    //Books active and rest position will effect other elements (paper and paperTextView)
+    self.bookRestPosition = CGRectMake((self.view.frame.size.width * 0.10), (self.view.frame.size.height * 0.4), self.view.frame.size.width, self.view.frame.size.height);
+    self.bookActivePosition = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y +100, self.view.frame.size.width, self.view.frame.size.height);
+    self.bookOpenPosition = CGRectMake(self.bookActivePosition.origin.x, self.bookActivePosition.origin.y, 20, self.bookActivePosition.size.height);
+
+
+    self.quillRestPosition = self.quill.frame;
+    self.quillActivePosition = CGRectMake(self.quill.frame.origin.x, self.view.frame.origin.y -200, self.quill.frame.size.width, self.quill.frame.size.height);
+
+    self.woodTableRestPosition = self.view.frame;
+    self.woodTableActivePosition = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y -200, self.view.frame.size.width, self.view.frame.size.height +200);
+
+
+    self.paperRestPosition = self.bookRestPosition;
+    self.paperActivePosition = CGRectMake(self.bookOpenPosition.size.width, self.bookActivePosition.origin.y, self.bookActivePosition.size.width, self.bookActivePosition.size.height);
+
+    self.paperTextViewRestPosition = self.bookRestPosition;
+    self.paperTextViewActivePosition = CGRectMake(self.paperActivePosition.origin.x, self.paperActivePosition.origin.y, self.paperActivePosition.size.width - 20, self.paperActivePosition.size.height);
+    self.paperTextView.editable = NO;
+
+
     self.paper.alpha = 0.0;
     self.paperTextView.alpha = 0.0;
-    self.bookStartPosition = self.logCoverImage.frame;
-    self.quillStartPosition = self.quill.frame;
+
+    self.paper.frame = self.bookRestPosition;
+    self.paperTextView.frame = self.bookRestPosition;
+
+
+
+
     [self animateAtStartup];
 
 }
@@ -49,27 +86,23 @@
 }
 
 - (IBAction)onBookTapped:(UITapGestureRecognizer *)sender {
-    if (self.logCoverImage.frame.origin.x == self.bookStartPosition.origin.x) {
+    if (self.logCover.frame.origin.x == self.bookRestPosition.origin.x) {
 
         [UIView animateWithDuration:1.0
                               delay:0.0
                             options:UIViewAnimationOptionCurveEaseInOut
                          animations:^{
-                             [self.logCoverImage setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-                             [self.quill setFrame:CGRectMake(self.quill.frame.origin.x, -200, self.quill.frame.size.width, self.quill.frame.size.height -200)];
-                             [self.woodTableImage setFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y -200, self.view.frame.size.width, self.view.frame.size.height +200)];
+                             [self activateLogAndViewsAssociated];
                          } completion:^(BOOL finished) {
                              NSLog(@"Book Slid up!");
                          }];
     }
-    else if (self.logCoverImage.frame.origin.x == 0){
+    else if (self.logCover.frame.origin.x == 0){
         [UIView animateWithDuration:1.0
                               delay:0.0
                             options:UIViewAnimationOptionCurveEaseInOut
                          animations:^{
-                             [self.logCoverImage setFrame:self.bookStartPosition];
-                             [self.quill setFrame:self.quillStartPosition];
-                             [self.woodTableImage setFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height)];
+                             [self everythingAtRestPosition];
                          } completion:^(BOOL finished) {
                              NSLog(@"Book Slid Down!");
                          }];
@@ -83,7 +116,8 @@
                               delay:0.0
                             options:UIViewAnimationOptionCurveEaseInOut
                          animations:^{
-                             [self.logCoverImage setFrame:CGRectMake(0, 0, 20, self.logCoverImage.frame.size.height)];
+                             [self.logCover setFrame:self.bookOpenPosition];
+
                          } completion:^(BOOL finished) {
                              NSLog(@"Book Has Opened!");
 
@@ -99,7 +133,8 @@
                           delay:0.0
                         options:UIViewAnimationOptionCurveEaseInOut
                      animations:^{
-                         [self.logCoverImage setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+                         [self.logCover setFrame:self.bookActivePosition];
+
                      } completion:^(BOOL finished) {
                          NSLog(@"Book Has Closed!");
                          //self.paper.alpha = 0.0;
@@ -110,29 +145,25 @@
                      }];
 }
 - (IBAction)onBookSwipeDown:(UISwipeGestureRecognizer *)sender {
-    if (self.logCoverImage.frame.origin.x == 0) {
+    if (self.logCover.frame.origin.x == 0) {
         [UIView animateWithDuration:1.0
                               delay:0.0
                             options:UIViewAnimationOptionCurveEaseInOut
                          animations:^{
-                             [self.logCoverImage setFrame:self.bookStartPosition];
-                             [self.quill setFrame:self.quillStartPosition];
-                             [self.woodTableImage setFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height)];
+                             [self everythingAtRestPosition];
                          } completion:^(BOOL finished) {
                              NSLog(@"Book Slid Down!");
                          }];
     }
 }
 - (IBAction)onBookSwipeUp:(UISwipeGestureRecognizer *)sender {
-    if (self.logCoverImage.frame.origin.x == self.bookStartPosition.origin.x) {
+    if (self.logCover.frame.origin.x == self.bookRestPosition.origin.x) {
 
         [UIView animateWithDuration:1.0
                               delay:0.0
                             options:UIViewAnimationOptionCurveEaseInOut
                          animations:^{
-                             [self.logCoverImage setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-                             [self.quill setFrame:CGRectMake(self.quill.frame.origin.x, -200, self.quill.frame.size.width, self.quill.frame.size.height -200)];
-                             [self.woodTableImage setFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y -200, self.view.frame.size.width, self.view.frame.size.height +200)];
+                             [self activateLogAndViewsAssociated];
                          } completion:^(BOOL finished) {
                              NSLog(@"Book Slid up!");
                          }];
@@ -141,28 +172,41 @@
 
 - (void)animateAtStartup
 {
-    self.logCoverImage.frame = CGRectMake(self.view.frame.size.width, self.view.frame.size.height, self.bookStartPosition.size.width, self.bookStartPosition.size.height);
+    self.logCover.frame = CGRectMake(self.view.frame.size.width, self.view.frame.size.height, self.bookRestPosition.size.width, self.bookRestPosition.size.height);
     [UIView animateWithDuration:1.0
                           delay:0.0
                         options:UIViewAnimationOptionCurveEaseInOut
                      animations:^{
-                         [self.logCoverImage setFrame:self.bookStartPosition];
-                         //[self.quill setFrame:self.quillStartPosition];
+                         [self.logCover setFrame:self.bookRestPosition];
                      } completion:^(BOOL finished) {
                          NSLog(@"Book Slid Down!");
                      }];
 }
 
-- (BOOL)textview:(UITextView *)textView{
-//    self.logCoverImage.alpha = 0.0;
-//    self.quill.alpha = 0.0;
-    return YES;
+
+
+
+
+- (void)everythingAtRestPosition
+{
+    [self.woodTable setFrame:self.woodTableRestPosition];
+    [self.logCover setFrame:self.bookRestPosition];
+    [self.paper setFrame:self.paperRestPosition];
+    [self.paperTextView setFrame:self.paperTextViewRestPosition];
+    [self.quill setFrame:self.quillRestPosition];
+
+}
+
+- (void) activateLogAndViewsAssociated
+{
+    [self.woodTable setFrame:self.woodTableActivePosition];
+    [self.logCover setFrame:self.bookActivePosition];
+    [self.paper setFrame:self.paperActivePosition];
+    [self.paperTextView setFrame:self.paperTextViewActivePosition];
+    [self.quill setFrame:self.quillActivePosition];
 }
 
 
 
-//- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    [self.view endEditing:YES];
-//}
 
 @end
